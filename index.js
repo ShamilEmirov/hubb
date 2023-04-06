869f175b59446f32d383af73ef92b7254cc4ef46
ShamilEmirov <ippon01@mail.ru>
2023-04-06 11:43:32 +0400

    addStringMethodFilter

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello JS");
+let users = 'Samil Ceyhun Oktay Sergiyye'
+console.log(users);
+users = users.split(" ");
+console.log(users);
+
+let dltName = document.getElementById("dltName");
+dltName.addEventListener('change', () => {
+    users = users.filter((name) => {
+        return name !== dltName.value;
+    })
+    console.log(users);
+    users = users.join(" ");
+    console.log(users);
+})
